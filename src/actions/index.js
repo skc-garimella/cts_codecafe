3b57eb073c189ea01eea327aed0a49d1a7240c54
skc-garimella <skc.garimella@gmail.com>
2016-10-04 22:15:28 -0500

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 const CODE_URL = 'https://hackerearth.0x10.info/api/ctz_coders?type=json&query=list_submissions';
-const IMAGE_URL = 'https://hackerearth.0x10.info/api/ctz_coders?type=json&query=list_compiler_image';
+const IMAGE_URL = 'http://hackerearth.0x10.info/api/ctz_coders?type=json&query=list_compiler_image';
 
 export const FETCH_CODE_SUBMISSIONS = 'FETCH_CODE_SUBMISSIONS';
 export const FILTER_CODE_LIST = 'FILTER_CODE_LIST';
